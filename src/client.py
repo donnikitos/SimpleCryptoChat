95d4f58b85f9ddcca918900c96e414c11f937e49
Nikita N <igelhaut94@gmail.com>
2018-11-09 08:35:31 +0100

    Update client.py

--- src/client.py
+++ src/client.py
@@ -4,17 +4,23 @@
 import sys
 import socket as sckt
 import select as slct
-from OpenSSL import SSL
+import re
 
 import defaults
 
-host = '127.0.0.1'
 
+host =  '104.248.196.50'
+if len(sys.argv) >= 2:
+	host =  sys.argv[1]
+
+print(len(sys.argv))
 
 sys.stdout.write('Choose your ID: ')
 username = raw_input()
 
 socket = sckt.socket(sckt.AF_INET, sckt.SOCK_STREAM)
+sys.stdout.write('Connecting to: ' + host + '\n')
+sys.stdout.flush()
 socket.connect((host, defaults.PORT))
 socket.send(defaults.protocol(username, 'server', '/set uid ' + username))
 
@@ -25,13 +31,20 @@ while running:
 	for inpt in read:
 		if inpt == socket:
 			data = inpt.recv(1024)
-			print(data)
+			if data:
+				result = re.match(defaults.PROTOCOL_RE, data)
+				print(result.group(3).rjust(45))
+			else:
+				print('Connection closed by server, exiting')
+				running = False
 		else:
 			msg = raw_input()
 			if msg[:1] == '/':
 				msg = msg[1:].split()
 				if msg[0] == 'user':
 					recepient = msg[1]
+				elif msg[0] == 'exit':
+					running = False
 			else:
 				socket.send(defaults.protocol(username, recepient, msg))
 
